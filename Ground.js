cee3a2e0c813abd5659dfff505cf16857c9b7384
nishtha373 <76241415+nishtha373@users.noreply.github.com>
2021-05-07 14:46:23 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,76 @@
+class Ground{
+  constructor(x,y,width,height){
+   var options = {
+      isStatic:true
+  }
+  this.body = Bodies.rectangle(x,y,width,height,options);
+  this.width=width;
+  this.height=height;
+  World.add(world,this.body);
+ }
+ display(){
+ var pos = this.body.position;
+ rectMode(CENTER);
+ fill(225);
+ rect(pos.x,pos.y,this.width,this.height);
+  }
+ };
+
+
+ //class Ground {
+  //constructor(x,y,width,height) {
+    //var options = {
+      //  isStatic: true
+    //}
+    //this.body = Bodies.rectangle(x,y,width,height,options);
+    //this.width = width;
+    //this.height = height;
+    //World.add(world, this.body);
+  //}
+  //display(){
+    //var pos =this.body.position;
+    //rectMode(CENTER);
+    //fill("white");
+    //rect(pos.x, pos.y, this.width, this.height);
+  //}
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
